42bccf9a3c1db90d4035c1a75acb691a44f7cdbd
zeroquinc <desiler@protonmail.com>
2023-10-21 21:19:11 +0200

    fix docker

--- src/docker.py
+++ src/docker.py
@@ -3,27 +3,21 @@ import json
 
 def run_docker_ps():
     try:
-        # Run the "docker ps --format json" command and capture the output
         output = subprocess.check_output(['docker', 'ps', '--format', 'json'], text=True)
-
-        # Parse the JSON output
-        container_info = json.loads(output)
-
-        # Extract the desired fields from the container information
+        lines = output.strip().split('\n')
         extracted_info = []
-        for container in container_info:
+        for line in lines:
+            container_info = json.loads(line)
             info = {
-                'Created At': container['CreatedAt'],
-                'Image': container['Image'],
-                'Names': container['Names'],
-                'Networks': container['Networks'],
-                'RunningFor': container['RunningFor'],
-                'Size': container['Size'],
-                'Status': container['Status']
+                'Created At': container_info['CreatedAt'],
+                'Image': container_info['Image'],
+                'Names': container_info['Names'],
+                'Networks': container_info['Networks'],
+                'RunningFor': container_info['RunningFor'],
+                'Size': container_info['Size'],
+                'Status': container_info['Status']
             }
             extracted_info.append(info)
-
         return extracted_info
-
     except subprocess.CalledProcessError as e:
         return f"Error: {e}"
