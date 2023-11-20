68e05ccc138894b067d60344c28c9b1834d3fdd3
zeroquinc <desiler@protonmail.com>
2023-11-21 00:09:58 +0100

    created trakt.py

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ from src.globals import bot, TOKEN, CHANNEL_PLEX_CONTENT, CHANNEL_PLEX_PLAYING, 
 from src.sonarr import create_sonarr_embed
 from src.radarr import create_radarr_embed
 from src.plex import create_plex_embed
+from src.trakt import create_weekly_global_embed, create_weekly_user_embed
 from src.tautulli import tautulli_discord_presence
 from src.logging import logger_discord, logger_trakt, logger_plex, logger_sonarr, logger_radarr, logger_tautulli
 
-import src.weekly_trakt_plays_user.main
 import src.weekly_trakt_plays_global.main
 import src.trakt_ratings_user.ratings
 import src.trakt_favorites.favorites
@@ -39,13 +39,13 @@ async def send(ctx, channel_id: int, *, message: str):
 async def send_weekly_embeds(ctx):
     try:
         # Weekly Trakt User Plays
-        data_user = src.weekly_trakt_plays_user.main.create_weekly_embed()
+        data_user = create_weekly_user_embed()
         channel_user = bot.get_channel(1046746288412176434)
         for embed in data_user['embeds']:
             await channel_user.send(embed=discord.Embed.from_dict(embed))
         await ctx.send("Weekly Trakt User Plays sent successfully.")
         # Weekly Trakt Global Plays
-        data_global = src.weekly_trakt_plays_global.main.create_weekly_embed()
+        data_global = create_weekly_global_embed()
         channel_global = bot.get_channel(1144085449007177758)
         for embed in data_global['embeds']:
             await channel_global.send(embed=discord.Embed.from_dict(embed))
--- src/trakt.py
+++ src/trakt.py
@@ -6,7 +6,7 @@ from datetime import datetime, timedelta
 
 from src.globals import load_dotenv, TRAKT_CLIENT_ID, TMDB_API_KEY, TRAKT_USERNAME
 
-def create_weekly_embed():
+def create_weekly_user_embed():
     # Calculate the date range for the last 7 days
     end_date = datetime.now()
     start_date = end_date - timedelta(days=7)
@@ -223,5 +223,130 @@ def create_weekly_embed():
     # Send the content message
     data = {
         'embeds': [movies_embed.to_dict(), episodes_embed.to_dict()]
+    }
+    return data
+
+def create_weekly_global_embed(): 
+    # Trakt API endpoints
+    movie_url = 'https://api.trakt.tv/movies/watched/period=weekly'
+    show_url = 'https://api.trakt.tv/shows/watched/period=weekly'
+
+    # Trakt API headers
+    headers = {
+        'Content-Type': 'application/json',
+        'trakt-api-version': '2',
+        "trakt-api-key": TRAKT_CLIENT_ID
+    }
+
+    # Define a dictionary to map ranking numbers to emojis
+    ranking_emojis = {
+        1: ":first_place:",
+        2: ":second_place:",
+        3: ":third_place:"
+    }
+
+    # Get top 10 most played movies and their play time
+    movie_response = requests.get(movie_url, headers=headers)
+    movies = sorted(movie_response.json(), key=lambda x: x['watcher_count'], reverse=True)
+
+    # Get top 10 most played shows and their play time
+    show_response = requests.get(show_url, headers=headers)
+    shows = sorted(show_response.json(), key=lambda x: x['watcher_count'], reverse=True)
+
+    # Fetch poster image from TMDB API
+    def fetch_image(item_type, item_id):
+        if item_type == 'movie':
+            url = f'https://api.themoviedb.org/3/movie/{item_id}?api_key={TMDB_API_KEY}&language=en-US'
+        elif item_type == 'show':
+            url = f'https://api.themoviedb.org/3/tv/{item_id}?api_key={TMDB_API_KEY}&language=en-US'
+        else:
+            return ''
+        response = requests.get(url)
+        if response.ok:
+            data = response.json()
+            poster_path = data.get('poster_path', '')
+            if poster_path:
+                return f'https://image.tmdb.org/t/p/w500/{poster_path}'
+        return ''
+
+    # Calculate dates for footer_text
+    today = datetime.utcnow()
+    previous_week_start = today - timedelta(days=7)
+    previous_week_end = today - timedelta(days=1)
+    footer_text = f"{previous_week_start.strftime('%a %b %d %Y')} to {previous_week_end.strftime('%a %b %d %Y')}"
+
+    # Calculate the ISO week number for the previous week
+    _, iso_week, _ = previous_week_start.isocalendar()
+
+    # Create embed JSON for movies
+    movie_embed = {
+        "color": 0xFEA232,
+        "fields": [],
+        "timestamp": datetime.utcnow().isoformat(),
+        "thumbnail": {
+            "url": "",
+        },
+        "author": {
+            "name": f"Trakt - Top Movies in Week {iso_week}",
+            "icon_url": "https://i.imgur.com/tvnkxAY.png"
+        },
+        "footer": {
+            "text": footer_text
+        }
+    }
+
+    # Add movies and their play count to movie embed JSON
+    for i, movie in enumerate(movies[:9]):
+        watcher_count = "{:,}".format(movie['watcher_count'])
+        image_url = fetch_image('movie', movie['movie']['ids']['tmdb'])
+        trakt_url = f"https://trakt.tv/movies/{movie['movie']['ids']['slug']}"
+        ranking_emoji = ranking_emojis.get(i + 1, "")  # Get the emoji for the ranking, or empty string if not in top 3
+        ranking_text = "" if i < 3 else f"{i+1}. "  # Remove ranking number for top 3 items
+        if not movie_embed["thumbnail"]["url"] and image_url:
+            movie_embed["thumbnail"]["url"] = image_url
+        movie_embed["fields"].append({
+            "name": f"{ranking_emoji} {ranking_text}{movie['movie']['title']} ({movie['movie']['year']})",
+            "value": f"[{watcher_count} watchers]({trakt_url})",
+            "inline": True
+        })
+
+    # Create embed JSON for shows
+    show_embed = {
+        "color": 0x328efe,
+        "fields": [],
+        "timestamp": datetime.utcnow().isoformat(),
+        "thumbnail": {
+        "url": "",
+        },
+        "author": {
+            "name": f"Trakt - Top Shows in Week {iso_week}",
+            "icon_url": "https://i.imgur.com/tvnkxAY.png"
+        },
+        "footer": {
+            "text": footer_text
+        }
+    }
+
+    # Add shows and their play count to show embed JSON
+    for i, show in enumerate(shows[:9]):
+        watcher_count = "{:,}".format(show['watcher_count'])
+        image_url = fetch_image('show', show['show']['ids']['tmdb'])
+        trakt_url = f"https://trakt.tv/shows/{show['show']['ids']['slug']}"
+        ranking_emoji = ranking_emojis.get(i + 1, "")  # Get the emoji for the ranking, or empty string if not in top 3
+        ranking_text = f"{i+1}. " if i >= 3 else ""  # Display ranking number for non-top 3 items
+        if not show_embed["thumbnail"]["url"] and image_url:
+            show_embed["thumbnail"]["url"] = image_url
+        show_embed["fields"].append({
+            "name": f"{ranking_emoji} {ranking_text}{show['show']['title']} ({show['show']['year']})",
+            "value": f"[{watcher_count} watchers]({trakt_url})",
+            "inline": True
+        })
+
+    # Combine the two embeds into one dictionary
+    combined_embeds = [movie_embed, show_embed]
+
+    # Create a dictionary for the combined message
+    data = {
+        "embeds": combined_embeds
     }
     return data
