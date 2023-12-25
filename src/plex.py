cc46ab3ff74534367674ff26c8b382d3d4b2b3e4
zeroquinc <desiler@protonmail.com>
2023-12-26 00:25:59 +0100

    Add function to process embed fields
    Description: This commit adds a new function `process_embed_fields()` to the `plex.py` file. This function is responsible for processing the fields of each embed in the `create_plex_embed()` function. Additionally, a new function `process_field_name()` is added to handle specific field names and modify their values if necessary.
    
    Changes:
    - Added `process_embed_fields()` function to process embed fields.
    - Added `process_field_name()` function to modify specific field values.
    - Updated `create_plex_embed()` function to call `process_embed_fields()`.

--- src/plex.py
+++ src/plex.py
@@ -8,6 +8,7 @@ def create_plex_embed(data):
         if data and 'embeds' in data:
             embeds = data['embeds']
             for embed in embeds:
+                process_embed_fields(embed)
                 embeds_list.append(embed)
                 logger.info(f"Created embed for {embed['title']} event")
             return {'embeds': embeds_list}, 200
@@ -17,3 +18,13 @@ def create_plex_embed(data):
     except Exception as e:
         logger.error(f"Error while processing JSON payload: {str(e)}")
         return {'error': "Internal server error"}, 500
+
+def process_embed_fields(embed):
+    if 'fields' in embed:
+        for field in embed['fields']:
+            process_field_name(field)
+
+def process_field_name(field):
+    if 'name' in field and field['name'] in ['Resumed Playing', 'Started Playing']:
+        if 'value' in field and field['value'].startswith('00:'):
+            field['value'] = field['value'][3:]
