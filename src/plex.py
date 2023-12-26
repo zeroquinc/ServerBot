f0e913ed91af37b092ce9908f5dd0b472c468b79
zeroquinc <desiler@protonmail.com>
2023-12-26 14:38:27 +0100

    Small name change

--- src/plex.py
+++ src/plex.py
@@ -26,6 +26,6 @@ def process_embed_fields(embed):
     return embed
 
 def process_field_name(field):
-    if 'name' in field and (field['name'].endswith('Resumed Playing') or field['name'].endswith('Started Playing')):
+    if 'name' in field and (field['name'].endswith('Now Streaming') or field['name'].endswith('Streaming Resumed')):
         if 'value' in field and field['value'].startswith('00:'):
             field['value'] = field['value'][3:]
